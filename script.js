3aeed8d5c1b6938c5c543c95ff9c17a331a2422d
malayproject <malay.s1006@gmail.com>
2022-02-19 10:49:43 +0530

    mobile version added with near final product

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var navDivEl, demoBtnEl;
+var navDivEl, demoBtnEl, navPopupMEl, burgerMEl, closeMEl;
 
 var setHoverClass = e => {
     e.preventDefault()
@@ -14,17 +14,41 @@ var removeHoverClass = e => {
         e.target.classList.remove('hover')
     }
 }
+var burgerHandler = e => {
+    e.preventDefault()
+    if(e.target.id === 'burgerM')   {
+        console.log(e.target.id)
+        burgerMEl.classList.add('clicked')
+        navPopupMEl.classList.add('clicked')
+        closeMEl.classList.remove('clicked')
+    }
+}
+
+var closeHandler = e => {
+    e.preventDefault()
+    if(e.target.id === 'closeM')   {
+        console.log(e.target.id)
+        burgerMEl.classList.remove('clicked')
+        navPopupMEl.classList.remove('clicked')
+        closeMEl.classList.add('clicked')
+    }
+}
 
 var setEventListeners = () => {
     navDivEl.addEventListener('mouseover', setHoverClass)
     demoBtnEl.addEventListener('mouseover', setHoverClass)
     navDivEl.addEventListener('mouseout', removeHoverClass)
     demoBtnEl.addEventListener('mouseout', removeHoverClass)
+    burgerMEl.addEventListener('touchend', burgerHandler)
+    closeMEl.addEventListener('touchend', closeHandler)
 }
 
 var getElements = () => {
     navDivEl = document.getElementById('navDiv')
     demoBtnEl = document.getElementById('demoBtn')
+    navPopupMEl = document.getElementById('navPopupM')
+    burgerMEl = document.getElementById('burgerM')
+    closeMEl = document.getElementById('closeM')
 }
 
 var init = () => {
